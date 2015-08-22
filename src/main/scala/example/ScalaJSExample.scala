5ad25586c83858347cb325c05e8e8b7d791c21a1
Li Haoyi <haoyi@dropbox.com>
2015-08-21 18:19:32 -0700

    Simple Ammonite

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -4,9 +4,16 @@ import org.scalajs.dom
 import org.scalajs.dom.html
 import scala.util.Random
 
-case class Point(x: Int, y: Int){
+case class Point(x: Double, y: Double){
   def +(p: Point) = Point(x + p.x, y + p.y)
-  def /(d: Int) = Point(x / d, y / d)
+  def -(p: Point) = Point(x - p.x, y - p.y)
+  def /(d: Double) = Point(x / d, y / d)
+  def *(d: Double) = Point(x * d, y * d)
+  import math._
+  def rotate(d: Double) = Point(
+    x * cos(d) - y * sin(d),
+    x * sin(d) + y * cos(d)
+  )
 }
 
 @JSExport
@@ -16,29 +23,31 @@ object ScalaJSExample {
     val ctx = canvas.getContext("2d")
                     .asInstanceOf[dom.CanvasRenderingContext2D]
 
-    var count = 0
-    var p = Point(0, 0)
-    val corners = Seq(Point(255, 255), Point(0, 255), Point(128, 0))
+    ctx.fillStyle = "white"
+    ctx.fillRect(0, 0, 255, 255)
+    ctx.fillStyle = "red"
 
-    def clear() = {
-      ctx.fillStyle = "black"
-      ctx.fillRect(0, 0, 255, 255)
+
+    val fullAngle = 5.5 * math.Pi
+    val interval = math.Pi / 8.2
+    val segmentWidth = math.Pi / 10
+    val size = 8.0
+    val offsetGrowth = 1.042
+    val thicknessRatio = 1.8
+    ctx.translate(128, 128)
+
+    for (i <- 0 until (fullAngle / interval).toInt) {
+      ctx.beginPath()
+      val p1 = Point(size * math.pow(offsetGrowth, i), 0).rotate(interval * i)
+      ctx.moveTo(p1.x, p1.y)
+      val p2 = p1 * thicknessRatio
+      ctx.lineTo(p2.x, p2.y)
+      val p3 = p2.rotate(segmentWidth)
+      ctx.lineTo(p3.x, p3.y)
+      val p4 = p1.rotate(segmentWidth)
+      ctx.lineTo(p4.x, p4.y)
+      ctx.closePath()
+      ctx.fill()
     }
-
-    def run = for (i <- 0 until 10){
-      if (count % 3000 == 0) clear()
-      count += 1
-      p = (p + corners(Random.nextInt(3))) / 2
-
-      val height = 512.0 / (255 + p.y)
-      val r = (p.x * height).toInt
-      val g = ((255-p.x) * height).toInt
-      val b = p.y
-      ctx.fillStyle = s"rgb($g, $r, $b)"
-
-      ctx.fillRect(p.x, p.y, 1, 1)
-    }
-
-    dom.setInterval(() => run, 50)
   }
 }
